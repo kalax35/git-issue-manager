2fadd076f99fa1cb4592d8c3fbefc0ac84cae108
Klaudia <k.jonca@outlook.com>
2025-08-15 08:21:36 +0200

    chore: init base application

---
+++ GitIssuesManager/GitIssueManager.Core/Interfaces/IIssueService.cs
@@ -0,0 +1,16 @@
+﻿using GitIssueManager.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIssueManager.Core.Interfaces
+{
+    public interface IIssueService
+    {
+        Task<Issue> CreateIssueAsync(string repo, string title, string description);
+        Task<Issue> UpdateIssueAsync(string repo, int issueId, string title, string description);
+        Task CloseIssueAsync(string repo, int issueId);
+    }
+}
---
+++ GitIssuesManager/GitIssueManager.Core/Models/Issue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIssueManager.Core.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+    }
+}
